90b9dd8cb25a795faf72b0d0913c76ad893fdeea
Antoine <antoine.boudet@epitech.eu>
2019-05-15 10:51:46 -0700

    update script

---
+++ script2.php
@@ -0,0 +1,18 @@
+<?php
+	try
+	{
+		$bdd = new PDO('mysql:host=antoinebhfant.mysql.db;dbname=antoinebhfant', 'antoinebhfant', 'AB1797sxiriuxovh', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+		$req = $bdd->prepare('INSERT INTO SavoirSiConnect (id, nb) VALUES (:id, :nb)');
+			$req->execute(array(
+			    'nb' => 2,
+			    'id' => NULL
+			));
+
+	}
+	catch (PDOException $e)
+	{    
+		echo 'Connexion échouée : ' . $e->getMessage();
+	}
+	//Déconnect
+	$bdd = null;
+?>
